194d06a2476240f3e34923cb2b14f4159554d23d
qhfka0090 <qhfka0090@naver.com>
2025-08-13 15:13:35 +0900

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV === 'production' ? '/profile/' : '/',
   plugins: [react()],
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
